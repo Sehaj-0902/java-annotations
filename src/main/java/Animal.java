4aa018f2cda7e1c00a6c88e3132d6507fd339770
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-04-04 15:08:46 +0530

    [Sehaj] ADD: Program to override makeSound() method using @Override

---
+++ src/main/java/Animal.java
@@ -0,0 +1,18 @@
+public class Animal {
+    public void makeSound() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    // Overriding make sound method
+    @Override
+    public void makeSound() {
+        System.out.println("Dog makes a sound.");
+    }
+
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        dog.makeSound();
+    }
+}
